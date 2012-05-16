c5ad0cc81dc35d2ee82575d8d8bf9344f987dd33
Ben Woosley <ben.woosley@gmail.com>
2012-05-16 15:37:39 -0700

    Move constrainer, deconstrainer and helpers into a subdir to group the common behavior

--- lib/sexy_pg_constraints.rb
+++ lib/sexy_pg_constraints.rb
@@ -1,10 +1,13 @@
 module SexyPgConstraints
   extend ActiveSupport::Autoload
   autoload :SchemaDefinitions
-  autoload :Constrainer
-  autoload :Deconstrainer
   autoload :Constraints
-  autoload :Helpers
+
+  autoload_under 'constrainers' do
+    autoload :Constrainer
+    autoload :Deconstrainer
+    autoload :Helpers
+  end
 end
 
 require 'sexy_pg_constraints/railtie' if defined?(Rails)
